2bf2a2ef5a9bb6ac9631bff5bf1e3bbffdb5f2f6
Charles <charles.jouenne@gmail.com>
2023-01-20 16:44:19 +0100

    Correction sur la banniere

--- src/pages/FicheLogement.jsx
+++ src/pages/FicheLogement.jsx
@@ -1,6 +1,12 @@
+// import { useParams } from "react-router-dom";
+// import loging from "../data/logements.json";
+
 function FicheLogement () {
+	// const { loging } = useParams();
+  	// const loging = products.find((loging) => loging.id === logingId);
 	return (
 		<div>FicheLogement</div>
+		
 	)
 }
 
